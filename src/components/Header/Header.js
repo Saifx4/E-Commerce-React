a1029c750d6757c0c8fcff768aeba7ee2c6f6662
Saifx4 <saifahamedx4@gmail.com>
2020-04-09 12:54:05 -1200

    Authentication done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,54 @@
+import React, {useRef, useEffect, useState } from 'react';
+import logo from '../../images/logo.png';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../Login/useAuth';
+
+
+// const usePrevious = (value) =>{
+//     const prev = useRef();
+
+//     useEffect(()=>{
+//             prev.current = value;
+//     },[value])
+
+//     return prev.current;
+// }
+const Header = () => {
+   
+    // const [count,setCount] = useState(0);
+    // const previous = usePrevious(count);
+    const auth = useAuth();
+    console.log(auth.user);
+    console.log(auth.user && auth.user.name);
+    
+    
+    
+   
+    
+    
+    return (
+        <div className='header'>
+            <img src={logo} alt=""/>
+                {/* <h1>Count:{count} Prev : {previous}</h1>
+                <button onClick={()=>setCount(count+1)}>+</button>
+                <button onClick={()=>setCount(count-1)}>-</button> */}
+            <nav>
+                <Link to="/shop">Shop</Link>        
+                <Link to="/review">Review</Link>
+                <Link to="/inventory">Manage Inventory</Link>
+                {
+                    auth.user && <span style={{color:'yellow'}}>{ auth.user.name}</span>
+                    
+                }
+                {
+                     auth.user ? <a href="/login">Sign Out</a>:
+                    <a href="/login">Sign in</a>
+                }
+                
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
